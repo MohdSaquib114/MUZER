990e4a49d073c264d74b8fdb23ecb5e3b9e28ffd
MohdSaquib114 <msmsaq114@gmail.com>
2024-09-08 22:29:36 +0530

    update

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -116,8 +116,8 @@ export async function POST(req: NextRequest) {
             }
         }
 
-        const thumbnails = res.thumbnail.thumbnails;
-        thumbnails.sort((a: {width: number}, b: {width: number}) => a.width < b.width ? -1 : 1);
+        const thumbnails = res.thumbnail?.thumbnails;
+        thumbnails?.sort((a: {width: number}, b: {width: number}) => a.width < b.width ? -1 : 1);
 
         const existingActiveStreams = await prismaClient.stream.count({
             where: {
@@ -142,7 +142,7 @@ export async function POST(req: NextRequest) {
                 extractedId,
                 type: "Youtube",
                 title: res.title ?? "Can't find video",
-                smallImg: (thumbnails.length > 1 ? thumbnails[thumbnails.length - 2].url : thumbnails[thumbnails.length - 1].url) ?? "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg",
+                smallImg: (thumbnails?.length > 1 ? thumbnails[thumbnails?.length - 2].url : thumbnails[thumbnails?.length - 1].url) ?? "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg",
                 bigImg: thumbnails[thumbnails.length - 1].url ?? "https://cdn.pixabay.com/photo/2024/02/28/07/42/european-shorthair-8601492_640.jpg"
             }
         });
